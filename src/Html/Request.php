a9904977dc450f6df7dd889f4f21e25f20136724
Gergely Nagy <152151245+ngergely0@users.noreply.github.com>
2024-09-24 12:22:54 +0200

    Add files via upload

--- src/Html/Request.php
+++ src/Html/Request.php
@@ -54,26 +54,71 @@ class Request {
 
     private static function deleteRequest(): void
     {
+        $id = self::getResourceId();
+        if (!id) {
+            Response::response([], 400, Response::STATUSES[400]);
+        }
         $resourceName = self::getResourceName();
-        $resourceId = self::getResourceId();
-        
         switch ($resourceName) {
             case 'counties':
+                $code = 404;
                 $db = new CountyRepository();
-                if ($resourceId) {
-                    $entity = $db->find($resourceId);
-                    if ($entity) {
-                        $db->delete($resourceId);
-                        Response::response([], 204);
-                    }   
-                } else {
-                    Response::response([], 404, $_SERVER['REQUEST_URI'] . " not found");
+                $result = $db->delete($id);
+                if ($result) {
+                    $code = 204;
                 }
+                Response::response([], $code);
                 break;
-            default:
-                Response::response([], 404, $_SERVER['REQUEST_URI'] . " not found");
+                default:
+                    Response::response([], 404,  $_SERVER['REQUEST_URI'] . " not found");
+            }
+               
+    }
+    private static function postRequest() {
+        $resource = self::getResourceName();
+        switch ($response) {
+            case 'counties':
+                $data = self::getRequestData();
+                if (isset($data['name'])) {
+                    $db = new CountyRepository();
+                    $newId = $db->create($data);
+                    $code = 201;
+                    if (!$newid) {
+                        $code = 400;
+                    }
+                }
+                Response::response(['id' => $newId], $code);
         }
     }
+
+    private static function putRequest() {
+        $resource = self::getResourceName();
+        switch ($response) {
+            case 'counties':
+                $data = self::getRequestData();
+                $id = $data['id'];
+                $db = new CountyRepository();
+                $entity = $db->find($id);
+                $code = 404;
+                if ($entity) {
+                    $result = $db->update(
+                        $id, ['name' => $data['name']]
+                    );
+                    if ($result) {
+                        $code = 201;
+                    }
+                }
+                Response::response([], $code);
+                break;
+            default:
+            Response::response([], 404, "$uri not found");
+    }
+}
+
+    private static function getRequestData(): ?array {
+        return json_decode(file_get_contents("php://input"), true);
+    }
+ 
  
  
 
--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -6,7 +6,7 @@ use App\Database\DB;
 
 class BaseRepository extends DB
 {
-    protected string $tableName;
+    protected $tableName;
 
     public function getAll(): array
     {
@@ -27,11 +27,48 @@ class BaseRepository extends DB
         
     }
 
+    public function update(int $id, array $data) {
+        $set = '';
+        foreach ($data as $field -> $value) {
+            if ($set > '') {
+                $set .= ", $field = '$value'";
+            } else
+                $set .= "$field = '$value'";
+        }
+        $query = "UPDATE `{$this->tableName}` SET %s WHERE id = $id;";
+        $query = sprintf($query, $set);
+        $this->mysqli->query($query);
+ 
+        return $this->find($id);
+    }
+
     public function delete($id): string
     {
-        $query = "SELECT * FROM `{$this->tableName}` WHERE id = {$id}";
-        return $query;
-        
+        $query = "DELETE * FROM `{$this->tableName}` WHERE id = {$id}";
+        return $this->mysqli->query($query);      
+    }
+
+    public function create($data): ?int {
+        $fields = '';
+        $values = '';
+        foreach ($data as $field => $value) {
+            if ($fields > '') {
+                $fields .= ',' . $field;
+            } else
+                $fields .= $field;
+           
+            if ($values > '') {
+                $values .= ',' . "'$values'";
+            } else
+                $values .= "'$values'";
+        }
+        $sql  = "INSERT INTO '%s' (%s) VALUES (%s)";
+        $sql = sprintf($sql, $this->tableName, $fields, $values);
+        $this->mysqli->query($sql);
+ 
+        $lastInserted = $this->mysqli->query("SELECT LAST_INSERT_ID() id;");
+ 
+        return $lastInserted['id'];
     }
 
 }
